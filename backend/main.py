780ae73048a645cbe51dbe3ec5c5c9f52b46cd7b
abderrahmanefrt <abderrahmaneferhat93@gmail.com>
2026-07-28 18:31:38 +0100

    bdd

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from routers import ocr, bg
+from routers import detect
+from database import Base, engine
+from models import users
 
 app = FastAPI()
 app.include_router(ocr.router)
 app.include_router(bg.router)
+app.include_router(detect.router)
+
+Base.metadata.create_all(bind=engine)
