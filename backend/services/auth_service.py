eef77848c06590884eddf60af259b1aaf778cf50
abderrahmanefrt <abderrahmaneferhat93@gmail.com>
2026-07-30 20:20:09 +0100

    auth3

--- backend/services/auth_service.py
+++ backend/services/auth_service.py
@@ -1,7 +1,10 @@
 from sqlalchemy.orm import Session
+from fastapi import Header, HTTPException, Depends
 from passlib.context import CryptContext
 from models.users import User
 from utils.apigen import generer_api_key
+from database import SessionLocal
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -14,3 +17,21 @@ def create_user(db: Session, email: str ,password: str) -> User:
     db.commit()
     db.refresh(new_user)
     return new_user
+
+def get_db():
+  db = SessionLocal()
+  try:
+      yield db
+  finally:
+      db.close()
+
+def verifier_api_key(x_api_key: str = Header(...), db: Session = Depends(get_db)) -> User:
+    utilisateur = db.query(User).filter(User.api_key == x_api_key).first()
+    
+    if not utilisateur:
+        raise HTTPException(status_code=401, detail="Clé API invalide")
+    
+    if utilisateur.credits <= 0:
+        raise HTTPException(status_code=402, detail="Crédits épuisés")
+    
+    return utilisateur
