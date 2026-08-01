7e3149cb7e10cda656e6253eab959efaa01081f7
abderrahmanefrt <abderrahmaneferhat93@gmail.com>
2026-08-01 19:26:19 +0100

    frontend

---
+++ frontend/src/components/Register.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import axios from "axios";
+
+interface RegisterResponse {
+  email: string;
+  api_key: string;
+}
+
+function Register() {
+  const [email, setEmail]= useState("");
+  const [password, setPassword]= useState("");
+  const [apikey, setApikey]= useState("");
+
+  const handleSubmit = async ()=> {
+    try{
+      const response= await axios.post<RegisterResponse>("http://localhost:8000/api/register",{
+        email,
+        password
+      })
+      setApikey(response.data.api_key);
+    }catch(error){
+      console.error("Erreur lors de l'inscription:", error);
+    }
+  }
+
+
+return (
+  <div>
+    <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email" />
+    <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Mot de passe" />
+    <button onClick={handleSubmit}>S'inscrire</button>
+    {apikey && <p>Votre clé API: {apikey}</p>}
+  </div>
+)
+}
+
+export default Register;
